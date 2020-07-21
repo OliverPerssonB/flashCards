67252e8cd5c2fb8b34582b72fb7972da2386875f
oliver_persson_b <oliver.persson.b@gmail.com>
2020-07-21 20:21:54 +0200

    Minor change

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-javac -d ./bin/ -cp "./src/:/usr/share/openjfx/lib/*" ./src/*.java
+javac -d ./bin/ -cp "./src/:/usr/share/openjfx/lib/*" ./src/*.java &&
 java -cp "./bin/" --module-path /usr/share/openjfx/lib --add-modules=javafx.controls,javafx.fxml,javafx.base,javafx.media,javafx.web,javafx.swing src.GUI
